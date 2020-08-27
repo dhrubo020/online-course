16bd75eaf7b2491f4bfd439ef279428802406032
dhrubo020 <singhadhrubo.cse3.bu@gmail.com>
2020-08-27 13:23:10 +0600

    icon added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import './Course.css'
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCartPlus, faLevelUpAlt, faStar,  faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Course = (props) => {
     //console.log("course", typeof (props.data))
@@ -23,10 +23,21 @@ const Course = (props) => {
                                     <Col md={10} >
                                         <div>
                                             <h5>{every.name}</h5>
-                                            <span>By : {every.by}</span>
+                                            
+                                            <FontAwesomeIcon icon={faStar} style={{color:'orange'}} />
+                                            <FontAwesomeIcon icon={faStar} style={{color:'orange'}}/>
+                                            <FontAwesomeIcon icon={faStar} style={{color:'orange'}}/>
+                                            <FontAwesomeIcon icon={faStar} style={{color:'orange'}}/>
+                                            <FontAwesomeIcon icon={faStarHalfAlt} style={{color:'orange'}}/>
+                                            <span> {every.rating} </span> 
+                                            <br/>
+                                            <FontAwesomeIcon icon={faLevelUpAlt} /> 
+                                            <span> {every.level} </span>
+                                            <br/>
+
+                                            <b>By : {every.by}</b>
                                             <h5>${every.price}</h5>
-                                            <span>{every.level}</span><br />
-                                            <span>Rating : {every.rating}</span> <br /> <span>{every.students}k students</span>
+                                             <span>{every.students}k students</span>
                                             <br></br>
                                             <Button className="button" onClick={() => props.clickHandle({ every })} > Enrol Now </Button>
                                         </div>
--- src/components/Enrolled/Enrolled.js
+++ src/components/Enrolled/Enrolled.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCartPlus, faLevelUpAlt, faStar,  faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 import './Enrolled.css'
 const Enrolled = (props) => {
     const items = props.data;
@@ -19,7 +21,12 @@ const Enrolled = (props) => {
                             </Col>
                             <Col sm={6}>
                                 <div>
-                                <p>{every.name}</p>
+                                <b>{every.name}</b> <br/>
+                                <FontAwesomeIcon icon={faStar} style={{color:'orange'}} />
+                                <FontAwesomeIcon icon={faStar} style={{color:'orange'}}/>
+                                <FontAwesomeIcon icon={faStar} style={{color:'orange'}}/>
+                                <FontAwesomeIcon icon={faStar} style={{color:'orange'}}/>
+                                <FontAwesomeIcon icon={faStarHalfAlt} style={{color:'orange'}}/>
                                 </div>
                             </Col>
                             <Col sm={3}>
